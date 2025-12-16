e8695154a1992cf395f55b599df7dfced6d7fd7a
LarissaRabello <larissarabellodasilva@gmail.com>
2025-12-16 14:56:56 -0300

    Segunda etapa - CRUD Categoria

---
+++ src/categoria/categoria.service.ts
@@ -0,0 +1,48 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { DeleteResult, ILike, Repository } from 'typeorm';
+import { Categoria } from './entities/categoria.entity';
+
+@Injectable()
+export class CategoriaService {
+  constructor(
+    @InjectRepository(Categoria)
+    private categoriaRepository: Repository<Categoria>,
+  ) {}
+
+  async create(categoria: Categoria): Promise<Categoria> {
+    return await this.categoriaRepository.save(categoria);
+  }
+
+  async findAll(): Promise<Categoria[]> {
+    return await this.categoriaRepository.find();
+  }
+
+  async findOne(id: number): Promise<Categoria> {
+    const categoria = await this.categoriaRepository.findOne({ where: { id } });
+
+    if (!categoria)
+      throw new HttpException('Categoria não encontrada!', HttpStatus.NOT_FOUND);
+
+    return categoria;
+  }
+
+  async findByDescricao(descricao: string): Promise<Categoria[]> {
+    return await this.categoriaRepository.find({
+      where: {
+        descricao: ILike(`%${descricao}%`),
+      },
+    });
+  }
+
+  async update(id: number, categoria: Categoria): Promise<Categoria> {
+    await this.findOne(id);
+    categoria.id = id;
+    return await this.categoriaRepository.save(categoria);
+  }
+
+  async remove(id: number): Promise<DeleteResult> {
+    await this.findOne(id);
+    return await this.categoriaRepository.delete(id);
+  }
+}
